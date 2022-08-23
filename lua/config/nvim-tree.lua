482824bc820226eac0a83afe106eb20435744a1e
Shady Alfred <shadyalfred98@gmail.com>
2022-08-23 21:16:36 +0200

    `chore` update nvim-tree with current directory automatically

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_set_keymap('n', '<Space>op', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 require('nvim-tree').setup {
-    hijack_netrw = false
+    hijack_netrw = false,
+    sync_root_with_cwd = true
 }
