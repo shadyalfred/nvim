a107a55c9a99851c342adab676d7945fee4c0834
Shady Alfred <shadyalfred98@gmail.com>
2022-09-04 10:13:54 +0200

    `feat` <Tab> and <S-Tab> for snippets

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -88,28 +88,16 @@ cmp.setup.cmdline(':', {
     })
 })
 
--- vim.cmd([[
--- " gray
--- highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
--- " blue
--- highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
--- highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
--- " light blue
--- highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
--- highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
--- highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
--- " pink
--- highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
--- highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
--- " front
--- highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
--- highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
--- highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
--- ]])
-
--- Setup lspconfig.
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
---     capabilities = capabilities
--- }
+-- Tab for vsnip
+vim.keymap.set(
+    { 'i', 's' },
+    '<Tab>',
+    "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'",
+    { expr = true }
+)
+vim.keymap.set(
+    { 'i', 's' },
+    '<S-Tab>',
+    "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'",
+    { expr = true }
+)
