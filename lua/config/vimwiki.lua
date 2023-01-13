25e34ac38b011ac7ddbd99cddff5970aa335adbd
Shady Alfred <shadyalfred98@gmail.com>
2023-01-13 22:46:33 +0200

    `fix` conflicting vimwiki bindings

--- lua/config/vimwiki.lua
+++ lua/config/vimwiki.lua
@@ -5,3 +5,7 @@ vim.g.vimwiki_list = {{
 }}
 
 vim.g.vimwiki_global_ext = 0
+
+vim.g.vimwiki_key_mappings = { all_maps = 0 }
+
+vim.keymap.set('n', '<Leader>ww', '<Plug>VimwikiIndex', { noremap = true, silent = true })
