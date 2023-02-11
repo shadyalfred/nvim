5571c1aebdad644622c95ccf5ee1abdf5a0ffaac
Shady Alfred <shadyalfred98@gmail.com>
2023-02-11 12:02:06 +0200

    `feat` vimwiki

--- lua/config/vimwiki.lua
+++ lua/config/vimwiki.lua
@@ -8,4 +8,14 @@ vim.g.vimwiki_global_ext = 0
 
 vim.g.vimwiki_key_mappings = { all_maps = 0 }
 
+vim.g.vimwiki_create_link = 1
+
 vim.keymap.set('n', '<Leader>ww', '<Plug>VimwikiIndex', { noremap = true, silent = true })
+
+vim.cmd[[autocmd FileType vimwiki UfoDetach]]
+vim.cmd[[autocmd FileType vimwiki inoremap <silent><buffer> <CR> <C-]><Esc>:VimwikiReturn 3 5<CR>]]
+vim.cmd[[autocmd FileType vimwiki nnoremap <silent><buffer> <CR> <Plug>VimwikiFollowLink]]
+vim.cmd[[autocmd FileType vimwiki nnoremap <silent><buffer> <C-CR> <Plug>VimwikiToggleListItem]]
+vim.cmd[[autocmd FileType vimwiki setlocal foldlevel=0]]
+
+vim.g.vimwiki_folding = 'expr'
