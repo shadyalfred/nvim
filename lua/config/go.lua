fc389c86da293465c77a93ac8c8a867c134eda53
Shady Alfred <shadyalfred98@gmail.com>
2023-03-23 21:25:43 +0200

    `feat` format go

--- lua/config/go.lua
+++ lua/config/go.lua
@@ -1,11 +1,13 @@
 vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
 
 require('go').setup({
-    lsp_inlay_hints = {
-        enable = false,
-    },
-    dap_debug_keymap = false,
-    trouble = true
+  lsp_inlay_hints = {
+    enable = false,
+  },
+  dap_debug_keymap = false,
+  gofmt = 'gofumpt',
+  trouble = true,
+  max_line_len = 80
 })
 
 vim.keymap.set('n', '<Leader>oi', '<Cmd> GoImport <CR>', { noremap = true, silent = true })
