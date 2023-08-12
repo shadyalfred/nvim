18bc460ca93b07fe7b12efb0221aa9ac58f4d434
Shady Alfred <shadyalfred98@gmail.com>
2023-08-12 21:51:23 +0300

    `formatting`

--- lua/config/vimwiki.lua
+++ lua/config/vimwiki.lua
@@ -1,8 +1,8 @@
 vim.g.vimwiki_list = {{
-    path = vim.env.HOME .. '/HDD/Obsidian',
-    index = 'Index',
-    syntax = 'markdown',
-    ext = '.md'
+  path   = vim.env.HOME .. '/HDD/Obsidian',
+  index  = 'Index',
+  syntax = 'markdown',
+  ext    = '.md'
 }}
 
 vim.g.vimwiki_global_ext = 0
