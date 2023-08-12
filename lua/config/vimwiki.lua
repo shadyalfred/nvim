f635c6d0b7d0e184824261a3cea5b5e52aa78a2f
Shady Alfred <shadyalfred98@gmail.com>
2023-08-12 21:51:30 +0300

    `feat` cd into Obsidian's vault

--- lua/config/vimwiki.lua
+++ lua/config/vimwiki.lua
@@ -11,7 +11,15 @@ vim.g.vimwiki_key_mappings = { all_maps = 0 }
 
 vim.g.vimwiki_create_link = 1
 
-vim.keymap.set('n', '<Leader>ww', '<Plug>VimwikiIndex', { noremap = true, silent = true })
+vim.keymap.set(
+  'n',
+  '<Leader>ww',
+  function()
+    vim.cmd[[cd ~/Obsidian]]
+    vim.cmd[[VimwikiIndex]]
+  end,
+  { noremap = true, silent = true }
+)
 
 vim.cmd[[autocmd FileType vimwiki UfoDetach]]
 vim.cmd[[autocmd FileType vimwiki inoremap <silent><buffer> <CR> <C-]><Esc>:VimwikiReturn 3 5<CR>]]
