293d710c552593e636f9dc11907b2f9fbe94c886
Shady Alfred <shadyalfred98@gmail.com>
2023-11-18 00:21:08 +0200

    `feat` flake for nix dev shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            lua-language-server
+          ];
+        };
+    });
+}
